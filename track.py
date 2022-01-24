2453927e473d6e3ffd2ff7444d28d4ced82edfb6
Miki Tebeka <miki@353solutions.com>
2022-01-24 16:33:34 +0200

    end of day 1

--- track.py
+++ track.py
@@ -26,7 +26,7 @@ def running_speed(df):
         df['lat'].shift(), df['lng'].shift(),
     )
 
-    time_hours = df['time'] / pd.Timedelta(hours=1)
+    time_hours = df['time'].diff() / pd.Timedelta(hours=1)
     return dist_km / time_hours
 
 
@@ -35,3 +35,17 @@ def plot_speed(date, speed_kmh):
     ax.set_xticks([])  # Remove "None"
     ax.set_ylabel(r'Running speed $\frac{km}{h}$')
     return ax
+
+# __name__ -> dunder name
+if __name__ == '__main__':
+    from argparse import ArgumentParser, FileType
+
+    parser = ArgumentParser(description='Generate box plot of running speed')
+    parser.add_argument('csv_file', help='CSV file', type=FileType('r'))
+    parser.add_argument('out_file', help='Output file', type=FileType('w'))
+    args = parser.parse_args()
+
+    df = load_track(args.csv_file.name)
+    speed = running_speed(df)
+    ax = plot_speed('2022-01-23', speed)
+    ax.figure.savefig(args.out_file.name)
